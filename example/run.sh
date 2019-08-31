73b258c80b7f040e76cf8b97f743c84a40a159c5
Roman Nix <rnix@yandex.ru>
2019-08-31 13:47:52 +0700

    Improve example script

--- example/run.sh
+++ example/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cat "$DIR/input.txt" | "$DIR/../bin/traveller.php"
+
+"$DIR/../bin/traveller.php" < "$DIR/input.txt" > "$DIR/output.txt" \
+  && diff "$DIR/expected_output.txt" "$DIR/output.txt" && echo "OK!" || echo "Wrong!"
